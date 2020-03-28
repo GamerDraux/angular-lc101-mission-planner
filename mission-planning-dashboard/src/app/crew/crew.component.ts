824929b2bf4f22f183e3857e69d108d8b641b5a6
gamerDraux <61572578+GamerDraux@users.noreply.github.com>
2020-03-28 12:42:55 -0500

    got crewlist set up with remove buttons, an add name field, and editable names in list

--- mission-planning-dashboard/src/app/crew/crew.component.ts
+++ mission-planning-dashboard/src/app/crew/crew.component.ts
@@ -6,10 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crew.component.css']
 })
 export class CrewComponent implements OnInit {
-  crew: string[] =["Jessica Watkins", "Raja Chari", "Jasmin Moghbeli"];
+  crew: object[] = [
+    {name: "Eileen Collins", firstMission: false},
+    {name: "Mae Jemison", firstMission: false},
+    {name: "Ellen Ochoa", firstMission: true}
+  ];
+  memberBeingEdited:object=null;
   constructor() { }
 
   ngOnInit() {
   }
 
+  addNewCrewMember(name:string, first:boolean) {
+    this.crew.push({name:name,firstMission:first});
+  }
+
+  removeCrewMember(member){
+    let index = this.crew.indexOf(member);
+    this.crew.splice(index,1);
+  }
+
+  editName(member, newName){
+    let index = this.crew.indexOf(member);
+    if(newName!==''){
+      this.crew[index].name=newName;
+    }
+  }
+
+  edit(member:object){
+    this.memberBeingEdited=member;
+  }
 }
