365a6fb758ebec377bce1161d9fed29120707796
gamerDraux <61572578+GamerDraux@users.noreply.github.com>
2020-03-28 15:08:23 -0500

    added editing to list items on experiments

--- mission-planning-dashboard/src/app/experiments/experiments.component.ts
+++ mission-planning-dashboard/src/app/experiments/experiments.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./experiments.component.css']
 })
 export class ExperimentsComponent implements OnInit {
+  experimentList:array =['Mars soil sample', 'Dig on that red sand, Baby!', 'Plant growth in habitat', 'Human bone density'];
+  experimentBeingEdited=null;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  addExperiment(newExperiment){
+    // tslint:disable-next-line: new-parens
+    this.experimentList.push(newExperiment);
+  }
+
+  switchToEditBlock(newName){
+    this.experimentBeingEdited=newName;
+  }
+
+  editExperiment(experimentBeingEdited, newName){
+    const index = this.experimentList.indexOf(experimentBeingEdited);
+    this.experimentList[index]=newName;
+  }
 }
