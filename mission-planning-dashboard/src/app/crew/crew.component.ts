1829b9bc37eb94352409a94d66de9df8461049c9
gamerDraux <61572578+GamerDraux@users.noreply.github.com>
2020-03-28 12:56:51 -0500

    added some comments

--- mission-planning-dashboard/src/app/crew/crew.component.ts
+++ mission-planning-dashboard/src/app/crew/crew.component.ts
@@ -11,29 +11,38 @@ export class CrewComponent implements OnInit {
     {name: "Mae Jemison", firstMission: false},
     {name: "Ellen Ochoa", firstMission: true}
   ];
-  memberBeingEdited:object=null;
+  memberBeingEdited: object = null;
   constructor() { }
 
   ngOnInit() {
   }
 
-  addNewCrewMember(name:string, first:boolean) {
-    this.crew.push({name:name,firstMission:first});
-  }
-
-  removeCrewMember(member){
-    let index = this.crew.indexOf(member);
-    this.crew.splice(index,1);
-  }
-
-  editName(member, newName){
-    let index = this.crew.indexOf(member);
-    if(newName!==''){
-      this.crew[index].name=newName;
+  addNewCrewMember(name: string, first: boolean) {
+    let alreadyInCrew = false;
+    for (let person of this.crew){
+      if (person.name === name){
+        alreadyInCrew = true;
+        alert('This person is already on the crew list!');
+      }
+    }
+    if (!alreadyInCrew) {
+      this.crew.push({name: name, firstMission: first});
     }
   }
 
-  edit(member:object){
-    this.memberBeingEdited=member;
+  removeCrewMember(member){
+    const index = this.crew.indexOf(member);
+    this.crew.splice(index, 1);
+  }
+
+  editName(member, newName){
+    const index = this.crew.indexOf(member);
+    if (newName !== ''){
+      this.crew[index].name = newName;
+    }
+  }
+
+  edit(member: object){
+    this.memberBeingEdited = member;
   }
 }
