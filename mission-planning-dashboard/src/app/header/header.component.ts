bb3a44017f2a5799dd711848a7f641c5e2edcdb5
gamerDraux <61572578+GamerDraux@users.noreply.github.com>
2020-03-28 11:35:44 -0500

    header changeable

--- mission-planning-dashboard/src/app/header/header.component.ts
+++ mission-planning-dashboard/src/app/header/header.component.ts
@@ -6,11 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  missionName:string="Mars 2030";
-  rocketName: string="Plasma Max";
+
+  missionName = 'Apollo 11';
+  rocketName = 'Saturn V';
+  editingRocket = false;
+  editingMission = false;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  updateMission(updatedName: string) {
+    this.missionName = updatedName;
+    this.editingMission = false;
+  }
+
+  updateRocket(updatedName: string) {
+    this.rocketName = updatedName;
+    this.editingRocket = false;
+  }
 }
