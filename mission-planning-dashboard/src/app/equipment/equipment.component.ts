8fc2830e3e5cb9871da2bacfa494f55614c8e5a9
gamerDraux <61572578+GamerDraux@users.noreply.github.com>
2020-03-28 13:01:58 -0500

    refactored equipment list for variable array input

--- mission-planning-dashboard/src/app/equipment/equipment.component.ts
+++ mission-planning-dashboard/src/app/equipment/equipment.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./equipment.component.css']
 })
 export class EquipmentComponent implements OnInit {
-
+  equipmentList:Array=['Rubber Baby Boucy Buggies', 'Habitat Dome', 'Drones', 'Food Containers', 'Oxygen Tanks'];
   constructor() { }
 
   ngOnInit() {
